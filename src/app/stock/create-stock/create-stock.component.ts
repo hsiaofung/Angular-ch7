cbd07eb1187675b045edcce9b82f562c0d08daa9
tim <tim_chou@chowsangsang.com>
2020-05-28 16:51:04 +0800

    update

--- src/app/stock/create-stock/create-stock.component.ts
+++ src/app/stock/create-stock/create-stock.component.ts
@@ -5,6 +5,9 @@ import {
   Validators,
   FormBuilder,
 } from '@angular/forms';
+import { Stock } from 'src/app/model/stock';
+
+let counter = 1;
 
 @Component({
   selector: 'app-create-stock',
@@ -17,10 +20,11 @@ export class CreateStockComponent {
   //   code: new FormControl(null, [Validators.required, Validators.minLength(2)]),
   //   price: new FormControl(0, [Validators.required, Validators.min(0)]),
   // });
-
+  private stock: Stock;
   public stockForm: FormGroup;
   constructor(private fb: FormBuilder) {
     this.createForm();
+    this.stock = new Stock('Test' + counter++, 'TST', 20, 10); // 初始化股票模型
   }
   get name() {
     return this.stockForm.get('name');
@@ -40,7 +44,24 @@ export class CreateStockComponent {
     });
   }
 
+  loadStockFromServer() {
+    this.stock = new Stock('Test' + counter++, 'TST', 20, 10);
+    const stockFormModel = Object.assign({}, this.stock);
+    delete stockFormModel.previousPrice;
+    delete stockFormModel.favorite;
+    this.stockForm.setValue(stockFormModel); // 以stock資料模型值設定整個表單模型
+  }
+  patchStockForm() {
+    this.stock = new Stock(`Test ${counter++}`, 'TST', 20, 10);
+    this.stockForm.patchValue(this.stock); // 以可用欄位更新表單模型。
+  }
+
+  resetForm() {
+    this.stockForm.reset(); // 重置表單到初始狀態
+  }
+
   onSubmit() {
-    console.log('Stock Form Value', this.stockForm.value);
+    this.stock = Object.assign({}, this.stockForm.value); // 不要直接指派表單模型給資料模型。而是複製它。
+    console.log('Saving stock', this.stock);
   }
 }
